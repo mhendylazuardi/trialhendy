5b497e626c9ab64cdbfd1b7a70907dcaee9ffbcc
mhendylazuardi <mochammad.lazuardi@students.amikom.ac.id>
2018-08-15 14:26:51 +0700

    done

--- advancedSearch/app/src/main/java/com/example/xback/search/MainActivity.java
+++ advancedSearch/app/src/main/java/com/example/xback/search/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.xback.search;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView search_book;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_book = (ListView) findViewById(R.id.search_book);
+
+        ArrayList<String> arrayBook = new ArrayList<>();
+        arrayBook.addAll(Arrays.asList(getResources().getStringArray(R.array.list_book)));
     }
 }
