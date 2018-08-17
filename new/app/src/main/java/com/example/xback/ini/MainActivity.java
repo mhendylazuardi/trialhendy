229fdd597337f5b60672e346fb5c84706d9a130a
mhendylazuardi <mochammad.lazuardi@students.amikom.ac.id>
2018-08-17 18:12:59 +0700

    done

--- new/app/src/main/java/com/example/xback/ini/MainActivity.java
+++ new/app/src/main/java/com/example/xback/ini/MainActivity.java
@@ -2,27 +2,31 @@ package com.example.xback.ini;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.example.xback.ini.model.Book;
+import android.app.SearchManager;
+import android.support.v7.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BookView {
     private String TAG = MainActivity.class.getSimpleName();
 
-    private ProgressDialog progressDialog;
-    private ListView listView;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
 //    URL to contacts JSON
     private static String URL = "https://www.googleapis.com/books/v1/volumes?q=%7Bkeyword";
-
-    ArrayList<HashMap<String, String>> booklist;
+    private BookAdapter bookAdapter;
+    private BookPresenter bookPresenter;
 
 
     @Override
@@ -30,53 +34,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        booklist = ArrayList<>();
-
-        listView = (ListView) findViewById(R.id.listViewMain);
-
-        new GetBooks().execute();
+        setUpComponent();
+        bookPresenter.getBook("Blablabla");
     }
 
-//    Async Task to call JSON
+    private void setUpComponent(){
+        bookPresenter       = new BookPresenter(this);
+        recyclerView        = (RecyclerView) findViewById(R.id.recycleViewBook);
+        swipeRefreshLayout  = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshBook);
+        bookAdapter         = new BookAdapter();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(bookAdapter);
+    }
 
-    private class GetBooks extends AsyncTask<Void, Void, Void>{
+    @Override
+    public void getBookSuccess(Book book) {
+//        masukan data ke adapter
+        bookAdapter.addBookItem(book.getItems());
+    }
 
-        @Override
-        protected Void onPrexecute(){
-            super.onPreExecute();
-//            showing progress dialog
-            progressDialog = new ProgressDialog(MainActivity.this);
-            progressDialog.setMessage("Loading");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-        }
+    @Override
+    public void getBookFailure(String errorMessage) {
+//        nampilin toast
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
 
-        @Override
-        protected Void doInBackground(Void... arg0){
-            HttpHandler sh = new HttpHandler();
+    @Override
+    public void getBookLoading(boolean isLoading) {
+//        laoding
+        swipeRefreshLayout.setRefreshing(isLoading);
 
-//            make a request to URL and getting response
-            String jsonString = sh.makeServiceCall(URL);
+    }
 
-            Log.e(TAG, "Response from URL: " + jsonString);
-
-            if (jsonString != null){
-                try {
-                    JSONObject jsonObject = new JSONObject(jsonString);
-
-//                    Getting array node
-                    JSONArray items = jsonObject.getJSONArray("items");
-                    int length = jsonObject.length();
-
-//                    looping
-                    for (int i = 0; i < length(); i++){
-                        JSONObject b = items.getJSONObject(i);
-
-                        String name = b.getString()
-                    }
-
-                }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        if (searchView != null)
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                bookPresenter.getBook(query);
+                return true;
             }
-        }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
