6728ba3288b721c3d54e736d757401295e793dcb
mhendylazuardi <mochammad.lazuardi@students.amikom.ac.id>
2018-09-03 10:05:07 +0700

    apa hayo

--- inikotlin/app/src/main/java/com/example/xback/inikotlin/RecyclerViewAdapter.kt
+++ inikotlin/app/src/main/java/com/example/xback/inikotlin/RecyclerViewAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.xback.inikotlin
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
 
-class RecyclerViewAdapter(private val context: Context)
+class RecyclerViewAdapter(private val context: Context, private val Items : List<Item>)
+    :RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>
